4f64bafec19d7379bd22ece7da40d19ca2e63b45
Sypen <nsypenj@pust2zzso.ukr.education>
2025-06-21 12:32:17 +0300

    style.css

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+STATIC_URL = '/static/'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -16,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking_app',
+    
 ]
 
 MIDDLEWARE = [
